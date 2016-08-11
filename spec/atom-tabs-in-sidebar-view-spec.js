4102494883311f7cf77faa0a950b154118f34541
Dan Kaplun <dbkaplun@gmail.com>
2016-08-11 15:46:14 -0700

    feat(init): populate repo

---
+++ spec/atom-tabs-in-sidebar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTabsInSidebarView from '../lib/atom-tabs-in-sidebar-view';
+
+describe('AtomTabsInSidebarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
