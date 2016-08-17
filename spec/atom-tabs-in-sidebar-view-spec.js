eebf51fd11303e0347326d73b8b206052374b275
Dan Kaplun <dbkaplun@gmail.com>
2016-08-17 16:35:33 -0700

    chore(misc): rm unused code

--- spec/atom-tabs-in-sidebar-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomTabsInSidebarView from '../lib/atom-tabs-in-sidebar-view';
-
-describe('AtomTabsInSidebarView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
